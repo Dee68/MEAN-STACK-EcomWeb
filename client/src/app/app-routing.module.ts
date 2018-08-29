22c5c7b00f87830c73c0a588b73541452f89b78e
dee68 <ddimie283@gmail.com>
2018-08-29 20:12:53 +0300

    correction to front-shop-page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 // tslint:disable-next-line:quotemark
-=======
->>>>>>> e2beb28377320fe0b235918ced6e7ce25e652a49
 import { RouterModule, Routes } from "@angular/router";
 // tslint:disable-next-line:quotemark
 import { NgModule } from "@angular/core";
@@ -11,21 +8,9 @@ import { ShopComponent } from "./components/shop/shop.component";
 // tslint:disable-next-line:quotemark
 const routes: Routes = [
   // tslint:disable-next-line:quotemark
-<<<<<<< HEAD
   { path: "", redirectTo: "shop", pathMatch: "full" },
   // tslint:disable-next-line:quotemark
   { path: "shop", component: ShopComponent }
-=======
-  { path: "shop", component: ShopComponent },
-  {
-    // tslint:disable-next-line:quotemark
-    path: "",
-    // tslint:disable-next-line:quotemark
-    redirectTo: "shop",
-    // tslint:disable-next-line:quotemark
-    pathMatch: "full"
-  }
->>>>>>> e2beb28377320fe0b235918ced6e7ce25e652a49
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,32 +1,21 @@
+// tslint:disable-next-line:quotemark
 import { AppRoutingModule } from "./app-routing.module";
+// tslint:disable-next-line:quotemark
 import { BrowserModule } from "@angular/platform-browser";
+// tslint:disable-next-line:quotemark
 import { NgModule } from "@angular/core";
 
-<<<<<<< HEAD
+// tslint:disable-next-line:quotemark
+// tslint:disable-next-line:quotemark
 import { AppComponent } from "./app.component";
+// tslint:disable-next-line:quotemark
 import { NavbarComponent } from "./components/navbar/navbar.component";
+// tslint:disable-next-line:quotemark
 import { ShopComponent } from "./components/shop/shop.component";
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, ShopComponent],
+  declarations: [AppComponent, ShopComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule],
-=======
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { ShopComponent } from './components/shop/shop.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    ShopComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
->>>>>>> e2beb28377320fe0b235918ced6e7ce25e652a49
   providers: [],
   bootstrap: [AppComponent]
 })
